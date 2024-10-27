2aa163aa10efab8ae534d687d74719908c8d06b6
docar <weused@gmail.com>
2024-10-27 05:11:34 +0500

    Neovim Conform Plugin Setup Changed

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -1,27 +1,27 @@
--- Define your existing formatter configurations
-local options = {
+-- Setup Conform with options
+require("conform").setup {
   formatters_by_ft = {
     python = { "autopep8" },
     bash = { "shfmt" },
     sh = { "shfmt" },
     lua = { "stylua" },
     go = { "goimports", "gofumpt", "goimports-reviser" },
-    javascript = { { "prettierd", "prettier" } },
-    typescript = { { "prettierd", "prettier" } },
-    javascriptreact = { { "prettierd", "prettier" } },
-    typescriptreact = { { "prettierd", "prettier" } },
-    vue = { { "prettierd", "prettier" } },
-    css = { { "prettierd", "prettier" } },
-    scss = { { "prettierd", "prettier" } },
-    less = { { "prettierd", "prettier" } },
-    html = { { "prettierd", "prettier" } },
-    json = { { "prettierd", "prettier" } },
-    jsonc = { { "prettierd", "prettier" } },
-    yaml = { { "prettierd", "prettier" } },
-    markdown = { { "prettierd", "prettier" } },
-    ["markdown.mdx"] = { { "prettierd", "prettier" } },
-    graphql = { { "prettierd", "prettier" } },
-    handlebars = { { "prettierd", "prettier" } },
+    javascript = { "prettierd", "prettier" },
+    typescript = { "prettierd", "prettier" },
+    javascriptreact = { "prettierd", "prettier" },
+    typescriptreact = { "prettierd", "prettier" },
+    vue = { "prettierd", "prettier" },
+    css = { "prettierd", "prettier" },
+    scss = { "prettierd", "prettier" },
+    less = { "prettierd", "prettier" },
+    html = { "prettierd", "prettier" },
+    json = { "prettierd", "prettier" },
+    jsonc = { "prettierd", "prettier" },
+    yaml = { "prettierd", "prettier" },
+    markdown = { "prettierd", "prettier" },
+    ["markdown.mdx"] = { "prettierd", "prettier" },
+    graphql = { "prettierd", "prettier" },
+    handlebars = { "prettierd", "prettier" },
   },
 
   format_on_save = function(bufnr)
@@ -33,9 +33,24 @@ local options = {
   end,
 }
 
--- Setup Conform with options
-require("conform").setup(options)
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = "Disable autoformat-on-save",
+  bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = "Re-enable autoformat-on-save",
+})
 
--- Create Neovim user commands for toggling auto formatting
-vim.api.nvim_command [[command! FormatDisable :lua require('configs.format').disable_autoformat()]]
-vim.api.nvim_command [[command! FormatEnable :lua require('configs.format').enable_autoformat()]]
+-- -- Create Neovim user commands for toggling auto formatting
+-- vim.api.nvim_command [[command! FormatDisable :lua require('configs.format').disable_autoformat()]]
+-- vim.api.nvim_command [[command! FormatEnable :lua require('configs.format').enable_autoformat()]]
